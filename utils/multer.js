4a597fb1c992671766f30d090a6b8151776fdce7
Jonathan2307 <gratigny.j@me.com>
2022-05-31 12:46:19 +0200

    limitation 2mo/image et commentaires

--- utils/multer.js
+++ utils/multer.js
@@ -1,6 +1,6 @@
 import multer from "multer"
 
-const maxSize = 4 * 1024 * 1024 // 4MB
+const maxSize = 2 * 1024 * 1024 // 2MB
 
 const MIME_TYPES = {
   'image/jpeg': 'jpg',
@@ -8,21 +8,25 @@ const MIME_TYPES = {
 }
 const upload = multer({
   storage: multer.diskStorage({
+    //sauvegarde dans le dossier images
     destination: (req, file, cb) => {
       cb(null, 'images')
     },
+    //renomme le fichier
     filename: (req, file, cb) => {
       const name = file.originalname.split(".").join("_")
       cb(null, `${Date.now()}-${name}.${MIME_TYPES[file.mimetype]}`)
     }
   }),
+  //verifie le type de fichier
   fileFilter: (req, file, cb) => {
-    if (file.mimetype.startsWith('image/')) {
+    if (file.mimetype.startsWith('image/') && MIME_TYPES[file.mimetype]) {
       cb(null, true)
     } else {
       cb(new AppError('Ce n\'est pas une image!', 400), false)
     }
   },
+  //limite la taille du fichier
   limits: {
     fileSize: maxSize
   }
